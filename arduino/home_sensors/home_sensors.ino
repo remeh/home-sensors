8740a0e86f86cc6e7ecd626768e8da23a9c72659
remeh <remeh@remeh.fr>
2015-10-18 20:55:18 +0200

    arduino: follow arduino IDE standard for directory/files.

--- arduino/home_sensors/home_sensors.ino
+++ arduino/home_sensors/home_sensors.ino
@@ -10,13 +10,14 @@ void setup() {
 }
 
 void loop() {
+  float res = 0.0f;
   // In one second, retrieve 5 times the voltage of the
   // MCP9701A put on the Arduino's pin.
   for (int i = 0; i < 5; i++) {  
     // Vref = 5.0    
     float reading = analogRead(MCP9701_ARDUINO_PIN)*5.0f/1024.0f;
     // the MCP9701A has 0° at 0.4mv
-    float temp0 = reading - 0.4f;
+    float temp = reading - 0.4f;
     // 1° per 19.5mv
     temp = temp/0.0195f;
     
