92ec6025fb63cf775efe84e8a8a0b97cac13538b
remeh <remeh@remeh.fr>
2015-11-01 17:54:32 +0100

    olimex: HTTP call to the server to store sensors data.

--- olimex/serial_listener.py
+++ olimex/serial_listener.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python2
 
+import httplib
+import json
 import time
 import serial
 import sys
 
+# configuration
+server = 'http://localhost:9000'
+sensorId = '1'
+header = 'X-Header'
+
+# global var containing the sensors values
 temp = 0
 light = 0
 presence = False
@@ -27,11 +35,26 @@ def process_line(data):
                 presence = False
 
     print('T%s L%s P%s' % (temp, light, presence))
+    call_server(temp, light, presence)
+
+def call_server(temp, light, presence):
+    global server
+    global sensorId
+
+    data = {'values':[{'type': 'temp', 'value': temp}, {'type': 'presence', 'value': 1}, {'type': 'light', 'value': light}]}
+    body = json.dumps(data)
+
+    http = httplib.HTTPConnection('remy.io', 9005, timeout=10)
+    headers = { "Content-Type": "application/json", header: 'OK' }
+    http.request('POST', '/api/hit/' + sensorId, body, headers)
+    http.close()
 
 def listener(argv):
     if len(argv) == 0:
         print('Not enough argument:\n python2 serial_listener.py port baudrate')
         sys.exit(1)
+
+    call_server(10, 5, 1)
 
     ser = serial.Serial(argv[0], argv[1])
 
